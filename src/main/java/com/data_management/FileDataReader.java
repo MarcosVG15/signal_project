b9084dff73bb6c223d7b2a20a9599d848d0aa2c4
MarcosVG15 <m.vargasgarcia@student.maastrichtuniversity.nl>
2025-04-04 11:50:19 +0200

    WEEK 1

--- src/main/java/com/data_management/FileDataReader.java
+++ src/main/java/com/data_management/FileDataReader.java
@@ -1,14 +1,4 @@
 package com.data_management;
 
-import java.io.IOException;
-
-public class FileDataReader implements DataReader{
-    public FileDataReader(String path){
-
-    }
-
-    @Override
-    public void readData(DataStorage dataStorage) throws IOException {
-
-    }
+public class FileDataReader {
 }
