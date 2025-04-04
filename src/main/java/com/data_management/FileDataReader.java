414c55ddba3aec820d2b1f5fc95cbdef69642617
MarcosVG15 <m.vargasgarcia@student.maastrichtuniversity.nl>
2025-04-04 12:12:39 +0200

    Week 1 - Issued Fixed

---
+++ src/main/java/com/data_management/FileDataReader.java
@@ -0,0 +1,14 @@
+package com.data_management;
+
+import java.io.IOException;
+
+public class FileDataReader implements DataReader{
+    public FileDataReader(String path){
+
+    }
+
+    @Override
+    public void readData(DataStorage dataStorage) throws IOException {
+
+    }
+}
--- src/test/java/data_management/DataStorageTest.java
+++ src/test/java/data_management/DataStorageTest.java
@@ -1,9 +1,12 @@
 package data_management;
 
 import static org.junit.jupiter.api.Assertions.*;
+import java.io.*;
+
+import com.data_management.DataReader;
+import com.data_management.DataStorage;
 import org.junit.jupiter.api.Test;
 
-import com.data_management.DataStorage;
 import com.data_management.PatientRecord;
 
 import java.util.List;
@@ -11,12 +14,18 @@ import java.util.List;
 class DataStorageTest {
 
     @Test
-    void testAddAndGetRecords() {
+    void testAddAndGetRecords() throws IOException {
         // TODO Perhaps you can implement a mock data reader to mock the test data?
-        // DataReader reader
-        DataStorage storage = new DataStorage(reader);
-        storage.addPatientData(1, 100.0, "WhiteBloodCells", 1714376789050L);
-        storage.addPatientData(1, 200.0, "WhiteBloodCells", 1714376789051L);
+        DataReader reader  = new DataReader() {
+            @Override
+            public void readData(DataStorage storage) throws IOException {
+                storage.addPatientData(1, 100.0, "WhiteBloodCells", 1714376789050L);
+                storage.addPatientData(1, 200.0, "WhiteBloodCells", 1714376789051L);
+            }
+        };
+        DataStorage storage = new DataStorage(null);
+        reader.readData(storage);
+
 
         List<PatientRecord> records = storage.getRecords(1, 1714376789050L, 1714376789051L);
         assertEquals(2, records.size()); // Check if two records are retrieved
