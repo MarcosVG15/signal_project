b3243bd45ce85ef115cc50494bfa712729960b22
MarcosVG15 <m.vargasgarcia@student.maastrichtuniversity.nl>
2025-04-26 14:20:55 +0200

    alerts are added to the AlertGenerator

--- src/main/java/com/alerts/AlertGenerator.java
+++ src/main/java/com/alerts/AlertGenerator.java
@@ -2,6 +2,14 @@ package com.alerts;
 
 import com.data_management.DataStorage;
 import com.data_management.Patient;
+import com.data_management.PatientRecord;
+
+import java.lang.annotation.Target;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * The {@code AlertGenerator} class is responsible for monitoring patient data
@@ -11,6 +19,14 @@ import com.data_management.Patient;
  */
 public class AlertGenerator {
     private DataStorage dataStorage;
+
+    private static final double SYSTOLIC_PRESSURE_MAX = 180 ;
+    private static final double SYSTOLIC_PRESSURE_MIN = 90 ;
+    private static final double DIASTOLIC_PRESSURE_MAX = 120 ;
+    private static final double DIASTOLIC_PRESSURE_MIN = 60 ;
+    private static final double OXYGEN_SATURATION_MIN = 92 ;
+
+
 
     /**
      * Constructs an {@code AlertGenerator} with a specified {@code DataStorage}.
@@ -35,7 +51,22 @@ public class AlertGenerator {
      * @param patient the patient data to evaluate for alert conditions
      */
     public void evaluateData(Patient patient) {
-        // Implementation goes here
+
+
+        Long endTime = System.currentTimeMillis();
+        Long startTime = endTime -(60*60*1000) ;
+        List<PatientRecord> records = patient.getRecords(startTime , endTime) ;
+
+        Alert alert = new Alert(null , null , 1) ;
+
+        bloodPressureDataAlert(records , alert);
+        bloodSaturationAlerts(records , alert) ;
+        hypotensiveHypoxemiaAlert(records , alert) ;
+        ECGAlert(records, alert);
+        ButtonEmergency(records ,alert);
+
+
+
     }
 
     /**
@@ -48,6 +79,201 @@ public class AlertGenerator {
      */
     private void triggerAlert(Alert alert) {
         // Implementation might involve logging the alert or notifying staff
-        
+        System.out.printf("Patient ID : %s | Condition : %s | TimeStamp: %d%n ",
+            alert.getPatientId(),
+            alert.getCondition(),
+            alert.getTimestamp()
+        );
     }
+
+    private List<PatientRecord> getSpecficValueRecord(String label , List<PatientRecord> records){
+        List<PatientRecord> specificPatientRecord = new ArrayList<>();
+
+        for (PatientRecord record : records){
+            if(record.getRecordType().equalsIgnoreCase(label)){
+                specificPatientRecord.add(record) ;
+            }
+        }
+
+        return specificPatientRecord ;
+    }
+
+    private void bloodPressureDataAlert( List<PatientRecord> records, Alert alert){
+
+        List<PatientRecord> systolic = getSpecficValueRecord("SystolicPressure" , records) ;
+        List<PatientRecord> diastolic = getSpecficValueRecord("DiastolicPressure" , records)  ;
+
+
+        double[] systolicArr = new double[4];
+        double[] diastolicArr = new double[4];
+
+        int i = 0;
+        while (records.size() > i) {
+            if (i < 4) {
+                systolicArr[i] = systolic.get(i).getMeasurementValue();
+                checkBloodPressure("SystolicPressure", systolicArr, alert);
+
+                diastolicArr[i] = diastolic.get(i).getMeasurementValue();
+                checkBloodPressure("DiastolicPressure", diastolicArr, alert);
+
+            } else {
+                for (int j = 1; j < diastolicArr.length; j++) {
+                    diastolicArr[j - 1] = diastolicArr[j];
+                    systolicArr[j-1] = systolicArr[j] ;
+                }
+                systolicArr[systolicArr.length - 1] = systolic.get(i).getMeasurementValue();
+                checkBloodPressure("SystolicPressure", systolicArr, alert);
+
+                diastolicArr[diastolicArr.length - 1] = diastolic.get(i).getMeasurementValue();
+                checkBloodPressure("DiastolicPressure", diastolicArr, alert);
+
+            }
+            i++;
+        }
+
+    }
+
+    private void checkBloodPressure(String label , double[] past3Records , Alert alert){
+
+        switch (label){
+            case ("SystolicPressure") :
+                if(Arrays.stream(past3Records).anyMatch(v -> v < SYSTOLIC_PRESSURE_MIN || v > SYSTOLIC_PRESSURE_MAX)){
+                    return ;
+                }
+                if(IntStream.range(1, past3Records.length)
+                        .filter(j -> Math.abs(past3Records[j] - past3Records[j - 1]) >= 10)
+                        .count() >=3) {
+
+                    triggerAlert(alert);
+                }
+            break ;
+            case ("DiastolicPressure"):
+                if(Arrays.stream(past3Records).anyMatch(v -> v < DIASTOLIC_PRESSURE_MIN || v > DIASTOLIC_PRESSURE_MAX)){
+                    return ;
+                }
+                if(IntStream.range(1, past3Records.length)
+                        .filter(j -> Math.abs(past3Records[j] - past3Records[j - 1]) >= 10)
+                        .count() >3) {
+
+                    triggerAlert(alert);
+                }
+            break;
+            default:
+                return;
+        }
+    }
+    private void bloodSaturationAlerts(List<PatientRecord> originalRecords, Alert alert){
+        // I assume that the records contain different types of informatio , which is why I filter the original records.
+        List<PatientRecord> records = getSpecficValueRecord("Saturation" , originalRecords) ;
+
+        double previousVal = records.get(0).getMeasurementValue() ;
+        long previousTime = records.get(0).getTimestamp() ;
+
+        int i = 1 ;
+        while ( i<records.size()){
+            double currentVal = records.get(i).getMeasurementValue() ;
+            if(currentVal<= OXYGEN_SATURATION_MIN){
+                triggerAlert(alert);
+                return;
+            }
+            else{
+                long currentTime = records.get(i).getTimestamp() ;
+
+                if(Math.abs(currentVal-previousVal)>5){
+                    triggerAlert(alert);
+                    return;
+                }
+                else if(Math.abs(previousTime - currentTime)>=1000*60*10){
+                    previousTime = currentTime ;
+                    previousVal = currentVal ;
+
+                }
+            }
+            i++;
+        }
+    }
+
+    private void hypotensiveHypoxemiaAlert(List<PatientRecord> records, Alert alert){
+        ArrayList<Boolean>TriggerSys  = new ArrayList<>();
+        ArrayList<Boolean>Trigger02  = new ArrayList<>();
+
+        int i = 0 ;
+        while(i <records.size()){
+
+            PatientRecord currentRecord = records.get(i) ;
+            switch (currentRecord.getRecordType()){
+                case("SystolicPressure") :
+                    if(currentRecord.getMeasurementValue()<=SYSTOLIC_PRESSURE_MIN){
+                        TriggerSys.add(true);
+                    }
+                break ;
+                case("Saturation"):
+                    if(currentRecord.getMeasurementValue()<=OXYGEN_SATURATION_MIN) {
+                        Trigger02.add(true);
+                    }
+                break ;
+            }
+            if(Trigger02.isEmpty() | TriggerSys.isEmpty()){
+                continue;
+            }
+            else if(Trigger02.getLast() & TriggerSys.getLast()){
+                triggerAlert(alert);
+            }
+
+            i++;
+        }
+
+    }
+
+    private void ECGAlert(List<PatientRecord> records, Alert alert){
+        List<PatientRecord> ECG = getSpecficValueRecord("WhiteBloodCells" , records);
+
+        int i = 0 ;
+        double rangeOfTriggering = 0.5 ;
+        double average = 0 ;
+
+        double [] window = new double[(int)ECG.size()/20];
+
+        while (i< ECG.size()){
+            if(i<window.length){
+                window[i] = ECG.get(i).getMeasurementValue();
+            }
+            else{
+                average  = getAverage(window) ;
+                window = Arrays.copyOfRange(window ,0, window.length-1 );
+                window[i] = ECG.get(i).getMeasurementValue() ;
+            }
+
+            if(Math.abs(window[i] - average) > rangeOfTriggering){
+                triggerAlert(alert);
+                return ;
+            }
+            i++;
+
+        }
+
+    }
+    private double getAverage(double[] window){
+        double average = 0 ;
+
+        for(double current :window){
+            average += current ;
+        }
+        return average/ window.length ;
+    }
+
+    private void ButtonEmergency(List<PatientRecord> records , Alert alert){
+        List<PatientRecord> emergencyButton = getSpecficValueRecord("EmergencyButton"  , records);
+
+        int i =0 ;
+        while(i<emergencyButton.size()){
+            if(emergencyButton.get(i).getMeasurementValue() ==1 ){
+                triggerAlert(alert);
+                return;
+            }
+        }
+
+
+    }
+
 }
--- src/main/java/com/data_management/FileDataReader.java
+++ src/main/java/com/data_management/FileDataReader.java
@@ -1,14 +1,51 @@
 package com.data_management;
 
 import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.nio.file.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class FileDataReader implements DataReader{
-    public FileDataReader(String path){
+public class FileDataReader implements DataReader {
+    private final Path path;
+
+    public FileDataReader(String path) {
+        this.path = Paths.get(path);
 
     }
 
     @Override
     public void readData(DataStorage dataStorage) throws IOException {
 
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(path, "*.txt")) {
+            for (Path file : stream) {
+                fileScanner(file, dataStorage);
+            }
+        }
     }
-}
+        public void fileScanner (Path fPath, DataStorage dataStorage) throws IOException {
+            Pattern style = Pattern.compile(" Patient ID: (\\d+), Timestamp: (\\d+), Label: (\\w+), Data: ([-+]?\\d*\\.?\\d+)"
+            );
+            Files.lines(fPath).forEach(line -> {
+                Matcher matches = style.matcher(line);
+                if (matches.matches()) {
+                    try {
+                        int patientId = Integer.parseInt(matches.group(1));
+                        double measurementValue = Integer.parseInt(matches.group(4));
+                        String recordType = (matches.group(3));
+                        long timestamp = Long.parseLong(matches.group(2));
+
+                        dataStorage.addPatientData(patientId, measurementValue, recordType, timestamp);
+
+                    } catch (NumberFormatException e) {
+                        System.out.println("Error in parsing" + line);
+                    }
+
+                }
+            });
+
+
+        }
+    }
+
